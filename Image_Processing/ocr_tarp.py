6d2a02933b1a0acdc32d6af9c42eb721128e2840
Hemanth Harikrishnan <49975886+hemanthh17@users.noreply.github.com>
2021-03-02 09:42:53 +0530

    Image Processing using Pytesseract
    Colab dependencies solved

--- Image_Processing/ocr_tarp.py
+++ Image_Processing/ocr_tarp.py
@@ -7,15 +7,34 @@ Original file is located at
     https://colab.research.google.com/drive/1XRbzpi2vUENhSVMGpv9oVfm3vZ00-v7o
 """
 
+!sudo apt install tesseract-ocr
 !pip install pytesseract
 
-from PIL import Image
 import pytesseract
+import shutil
+import os
+import random
+try:
+ from PIL import Image
+except ImportError:
+ import Image
 import numpy as np
 import cv2
 from google.colab.patches import cv2_imshow
 
 filename = 'TARP1.jfif'
+img1 = np.array(Image.open(filename))
+cv2_imshow(img1)
+text = pytesseract.image_to_string(img1)
+print(text)
+
+filename = 'TARP2.jfif'
+img1 = np.array(Image.open(filename))
+cv2_imshow(img1)
+text = pytesseract.image_to_string(img1)
+print(text)
+
+filename = 'TARP3.jfif'
 img1 = np.array(Image.open(filename))
 cv2_imshow(img1)
 text = pytesseract.image_to_string(img1)
